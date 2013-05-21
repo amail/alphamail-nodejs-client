4c9e0014925f4ae0e829495d6f0aae61631f4081
Robin Orheden <robin@zapzapgo.com>
2013-05-22 01:23:32 +0200

    Updated library

--- src/lib/alphamail/index.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = {
-    Logger: require('./logger').Logger,
-    ServiceFactory: require('./servicefactory').ServiceFactory,
-    SignatureService: require('./signatureservice').SignatureService,
-    ProjectService: require('./projectservice').ProjectService,
-    EmailService: require('./emailservice').EmailService,
-    EmailMessagePayload: require('./emailservice').EmailMessagePayload,
-    EmailContact: require('./emailservice').EmailContact
-};
